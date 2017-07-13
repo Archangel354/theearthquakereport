ed27caf515b39cd4980fdac96767041a947fa63f
Archangel354 <rduncan354@yahoo.com>
2017-07-13 19:49:57 -0400

    This is a mess when you try to implement loaders

--- app/src/main/java/com/example/owner/theearthquakereport/EarthquakeLoader.java
+++ app/src/main/java/com/example/owner/theearthquakereport/EarthquakeLoader.java
@@ -2,18 +2,42 @@ package com.example.owner.theearthquakereport;
 
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
 
 /**
  * Created by Owner on 7/13/2017.
  */
 
-public class EarthquakeLoader  extends AsyncTaskLoader<Event>{
-    public EarthquakeLoader(Context context) {
+public class EarthquakeLoader  extends AsyncTaskLoader<List<Earthquake>>{
+
+    /** Query URL */
+    private String mUrl;
+
+
+
+    public EarthquakeLoader(Context context, String url) {
         super(context);
+        mUrl = url;
+    }
+
+
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
     }
 
     @Override
-    public Event loadInBackground() {
-        return null;
+    public List<Earthquake> loadInBackground() {
+
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = Utils.fetchEarthquakeData(mUrl);
+        return earthquakes;
     }
 }
