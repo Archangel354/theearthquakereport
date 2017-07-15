4bd2eea4b6e2272b7e21cfaa749fa8868f043ce5
Archangel354 <rduncan354@yahoo.com>
2017-07-15 14:07:18 -0400

    This is for the branch lists.  I added a return null statement to the onCreateLoader method

--- app/src/main/java/com/example/owner/theearthquakereport/MainActivity.java
+++ app/src/main/java/com/example/owner/theearthquakereport/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends AppCompatActivity  implements LoaderManager.Lo
     @Override
     public Loader<List<Earthquake>> onCreateLoader(int i, Bundle bundle) {
         // TODO: Create a new loader for the given URL
+        return  null;
 
     }
 
